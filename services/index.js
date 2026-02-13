07aa35d0c41ad112c6c8ccf94a02f356c56d5e2b
ghost <ghst-0@users.noreply.github.com>
2026-02-14 04:08:13 +0700

    Remove paid services

--- services/index.js
+++
@@ -1,9 +0,0 @@
-const advertise = require('./advertise');
-const servicePaidRequests = require('./service_paid_requests');
-const usePaidService = require('./use_paid_service');
-
-module.exports = {
-  advertise,
-  servicePaidRequests,
-  usePaidService,
-};
