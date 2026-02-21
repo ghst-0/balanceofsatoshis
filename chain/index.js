4d260a216d338145156ae3bb5e5c21215503d1cf
ghost <ghst-0@users.noreply.github.com>
2026-02-21 07:45:20 +0700

    Remove get channel closes Remove bolt03

--- chain/index.js
+++ chain/index.js
@@ -1,12 +1,10 @@
 import getChainFees from './get_chain_fees.js';
-import getChannelCloses from './get_channel_closes.js';
 import getUtxos from './get_utxos.js';
 import outputScriptForAddress from './output_script_for_address.js';
 import splitUtxos from './split_utxos.js';
 
 export {
   getChainFees,
-  getChannelCloses,
   getUtxos,
   outputScriptForAddress,
   splitUtxos
